3e865fbf8b1ca8c0fbf6454565dda11b9b3a4898
GOPINATH <ngopi6769@gmail.com>
2026-04-23 17:16:31 +0530

    commit

---
+++ src/test/java/org/edu/edutrack/EduTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edu.edutrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
